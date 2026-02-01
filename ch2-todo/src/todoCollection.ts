53069b75a5444658d5fd29dc28377d42a64dd532
Łukasz Kos <kos.lukasz.88@gmail.com>
2026-02-01 20:36:32 +0100

    Ch2: Completed

---
+++ ch2-todo/src/todoCollection.ts
@@ -0,0 +1,53 @@
+import { TodoItem } from "./todoItem.js";
+
+type ItemCounts = {
+  total: number;
+  incomplete: number;
+};
+
+export class TodoCollection {
+  private nextId = 1;
+  protected itemMap = new Map<number, TodoItem>();
+  constructor(
+    public userName: string,
+    todoItems: TodoItem[] = [],
+  ) {
+    todoItems.forEach((item) => this.itemMap.set(item.id, item));
+  }
+
+  addTodo(task: string): number {
+    while (this.getTodoById(this.nextId)) {
+      this.nextId++;
+    }
+    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
+    return this.nextId;
+  }
+
+  getTodoById(id: number): TodoItem {
+    return this.itemMap.get(id);
+  }
+  getTodoItems(includeCompleate: boolean): TodoItem[] {
+    return [...this.itemMap.values()].filter(
+      (item) => includeCompleate || !item.complete,
+    );
+  }
+  markComplete(id: number, complete: boolean) {
+    const item = this.getTodoById(id);
+    if (item) {
+      item.complete = complete;
+    }
+  }
+  removeComplete() {
+    this.itemMap.forEach((item) => {
+      if (item.complete) {
+        this.itemMap.delete(item.id);
+      }
+    });
+  }
+  getItemCounts(): ItemCounts {
+    return {
+      incomplete: this.getTodoItems(false).length,
+      total: this.itemMap.size,
+    };
+  }
+}
