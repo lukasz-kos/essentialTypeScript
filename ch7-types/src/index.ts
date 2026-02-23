84a6db57b680a57e2c294ab872257428d602051d
Łukasz Kos <kos.lukasz.88@gmail.com>
2026-02-23 21:56:59 +0100

    Ch11: In progress...

--- ch7-types/src/index.ts
+++ ch7-types/src/index.ts
@@ -1,13 +1,43 @@
-type Person = {
-  id: string;
+interface Person {
   name: string;
-  city: string;
-};
-let data: Person[] = [
-  { id: "bsmith", name: "Bob Smith", city: "London" },
-  { id: "ajones", name: "Alice Jones", city: "Paris" },
-  { id: "dpeters", name: "Dora Peters", city: "New York" },
+  getDetails(): string;
+}
+interface Product {
+  name: string;
+  price: number;
+}
+
+class Employee implements Person {
+  constructor(
+    public name: string,
+    public company: string,
+  ) {
+    // no statements required
+  }
+  getDetails() {
+    return `${this.name} works for ${this.company}`;
+  }
+}
+class SportsProduct implements Product {
+  constructor(
+    public name: string,
+    public category: string,
+    public price: number,
+  ) {
+    // no statements required
+  }
+}
+
+let data: (Person | Product)[] = [
+  new Employee("Bob Smith", "Acme"),
+  new SportsProduct("Running Shoes", "Running", 90.5),
+  new Employee("Dora Peters", "BigCo"),
 ];
+
 data.forEach((item) => {
-  console.log(`${item.id} ${item.name}, ${item.city}`);
+  if ("getDetails" in item) {
+    console.log(`Person: ${item.getDetails()}`);
+  } else {
+    console.log(`Product: ${item.name}, ${item.price}`);
+  }
 });
