6910f739c8e56f37a5d97f098422446cb1ffb05c
Łukasz Kos <kos.lukasz.88@gmail.com>
2026-03-06 22:55:34 +0100

    Ch12: Completed

--- ch7-types/src/index.ts
+++ ch7-types/src/index.ts
@@ -1,9 +1,23 @@
-import { Person, Product } from "./dataTypes.js";
-
-let people = [
-  new Person("Bob Smith", "London"),
-  new Person("Dora Peters", "New York"),
-];
+import { City, Person, Product, Employee } from "./dataTypes.js";
 
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-[...people, ...products].forEach((item) => console.log(`Item: ${item.name}`));
+
+type shapeType = { name: string };
+
+class Collection<T extends shapeType> {
+  constructor(private items: T[] = []) {}
+  add(...newItems: T[]): void {
+    this.items.push(...newItems);
+  }
+  get(name: string): T {
+    return this.items.find((item) => item.name === name);
+  }
+  get count(): number {
+    return this.items.length;
+  }
+}
+
+let productCollection: Collection<Product> = new Collection(products);
+console.log(`There are ${productCollection.count} products`);
+let p = productCollection.get("Hat");
+console.log(`Product: ${p.name}, ${p.price}`);
